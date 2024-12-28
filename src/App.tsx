74949f784d0bfd32170d690d6ea5cfa5c63d0dd7
skidiko <dvkobets@yandex.ru>
2024-12-28 18:56:52 +0300

    ready project with build

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import PostList from './pages/PostList.tsx';
 import PostDetails from './pages/PostDetails.tsx';
 
 const App: React.FC = () => {
     return (
-        <Router>
             <Routes>
                 <Route path="/" element={<PostList />} />
                 <Route path="/post/:id" element={<PostDetails />} />
             </Routes>
-        </Router>
     );
 };
 
